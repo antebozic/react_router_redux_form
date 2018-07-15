beb94a0aec4448761e295fa498822e8fd3acf8e9
ANTE BOZIC <antebozic@gmail.com>
2018-07-15 19:38:40 +0200

    installed redux-form

--- src/components/posts_index.js
+++ src/components/posts_index.js
@@ -2,7 +2,8 @@ import _ from 'lodash';
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { fetchPosts } from '../actions';
-
+import { Link } from 'react-router-dom';
+ 
 class PostsIndex extends Component {
     componentDidMount() {
         this.props.fetchPosts(); //kicks off data loading process
@@ -22,6 +23,11 @@ class PostsIndex extends Component {
         console.log(this.props.posts);
         return (
             <div>
+                <div className="text-xs-right">
+                    <Link className="btn btn-primary" to="/posts/new">
+                      Add a Post
+                    </Link>
+                </div>
             <h3>Posts</h3>
             <ul className="list-group">
                 {this.renderPosts()}
