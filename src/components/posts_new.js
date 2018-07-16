3af4a5ed918a50556403329f5c9d25c9bae820cb
ANTE BOZIC <antebozic@gmail.com>
2018-07-16 17:52:43 +0200

    form submittal

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -16,9 +16,16 @@ class PostsNew extends Component {
         );
     }
 
+    onSubmit(values) {
+        // this === component
+        console.log(values);
+    }
+
     render() {
+        const { handleSubmit } = this.props;
+
         return (
-            <form>
+            <form onSubmit={handleSubmit(this.onSubmit.bind(this))}>
                 <Field
                   label="Title for Post"
                   name="title"
@@ -34,6 +41,7 @@ class PostsNew extends Component {
                   name="content"
                   component={this.renderField}
                 />
+                <button type="submit" className="btn btn-primary">Submit</button>
             </form>
         );
     }
@@ -44,7 +52,7 @@ function validate(values) {
     const errors = {}
 
     // Validate inputs from 'values'
-    if (!values.title || values.title.length > 3) {
+    if (!values.title || values.title.length < 3) {
         errors.title = "Enter a title that is at least 3 characters!";
     }
 
