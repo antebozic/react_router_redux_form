9232a5446d64684dcc6c95fcac77f97424514156
ANTE BOZIC <antebozic@gmail.com>
2018-07-23 22:19:47 +0200

    post component, post action creator, navigation

--- src/components/post_show.js
+++ src/components/post_show.js
@@ -1,14 +1,35 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { fetchPost } from '../actions/';
+import { Link } from 'react-router-dom';
 
-class PostShow extends Component {
+class PostsShow extends Component {
+    componentDidMount() {
+        // this.props.match.params.id;
+        const { id } = this.props.match.params;
+        this.props.fetchPost(id);
+    }
 
     render() {
+        const { post } = this.props; //deconstructing
+
+        if(!post) {
+            return <div>Loading...</div>;
+        }
+
         return(
             <div>
-                Posts Show!
+                <Link to="/">Back to Index</Link>
+                <h3>{post.title}</h3>
+                <h6>Categories: {post.categories}</h6>
+                <p>{post.content}</p>
             </div>
         );
-    };
+    }
 }
 
-export default PostShow;
+function mapStateToProps({ posts }, ownProps){
+    return { post: posts[ownProps.match.params.id] };
+}
+
+export default connect(mapStateToProps, { fetchPost })(PostsShow);
